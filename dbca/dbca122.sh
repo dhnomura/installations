987c047740fd580d687128853b9ec58ffcd89905
Diogo Hikaru Nomura <62851638+dhnomura@users.noreply.github.com>
2023-02-17 16:26:26 -0300

    new file:   Infrastructure/BastionHost.yaml 	modified:   Infrastructure/EC2.yaml 	new file:   Infrastructure/EC2Win.yaml 	new file:   RSP/dbca122.rsp 	new file:   dbca/dbca122.sh

---
+++ dbca/dbca122.sh
@@ -0,0 +1,1 @@
+dbca -silent -createDatabase -templateName General_Purpose.dbc -gdbName db12 -sid db12 -createAsContainerDatabase false -emConfiguration NONE -datafileDestination $ORACLE_HOME/oradata -storageType FS -characterSet AL32UTF8 -totalMemory 4096 -recoveryAreaDestination /fra -sampleSchema true
